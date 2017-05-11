f286333cb5e04578bfb362f86f12cb4be181030f
Gregory White <gwhite@fairwaytech.com>
2017-05-11 11:13:09 -0700

    String & XmlElement Extensions tests

---
+++ STSCommon.Test/Extensions/StringExtensionsTest.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using STSCommon.Extensions;
+
+namespace STSCommon.Test.Extensions
+{
+    [TestFixture]
+    public class StringExtensionsTest
+    {
+        [Test]
+        public void ProduceSingleAttributeImgElementFromImgElementTest()
+        {
+            // Arrange
+            const string testString = "<img width=313 height=169 src = \"STS_RLASimulatedItems_files/image001.png\" />";
+
+            // Act
+            var result = testString.ProduceImgElementWithSourceFromStringElement("puppy.jpg");
+
+            // Assert
+            Assert.AreEqual(result, "<img src=\"puppy.jpg\">");
+        }
+
+        [Test]
+        public void RemoveSpecialCharactersTest()
+        {
+            // Arrange
+            const string testString = "~!@#$%^&*()4WEaZ..,;: ";
+
+            // Act
+            var result = testString.RemoveSpecialCharacters();
+
+            // Assert
+            Assert.AreEqual(result, "4WEaZ..");
+        }
+
+        [Test]
+        public void RestrictToSingleWhitespaceTest()
+        {
+            // Arrange
+            const string testString = "  TEST  2Space   ThreeSpace  \n\t";
+
+            // Act
+            var result = testString.RestrictToSingleWhiteSpace();
+
+            // Assert
+            Assert.AreEqual(result, "TEST 2Space ThreeSpace");
+        }
+    }
+}
---
+++ STSCommon.Test/Extensions/XmlElementExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+using NUnit.Framework;
+using STSCommon.Extensions;
+
+namespace STSCommon.Test.Extensions
+{
+    [TestFixture]
+    public class XmlElementExtensions
+    {
+        [Test]
+        public void TestAddingChildElement()
+        {
+            // Arrange
+            var document = new XmlDocument();
+            var parentNode = document.CreateElement("test");
+            const string nodeName = "img";
+            var attributes = new Dictionary<string, string>
+            {
+                {
+                    "src",
+                    "puppy.jpg"
+                },
+                {
+                    "height",
+                    "720"
+                }
+            };
+
+            // Act
+            parentNode.AppendChild(document, nodeName, string.Empty, attributes);
+
+            // Assert
+            Assert.IsTrue(parentNode.HasChildNodes);
+            Assert.AreEqual(parentNode.FirstChild.Name, nodeName);
+            Assert.IsTrue(((XmlElement) parentNode.FirstChild).HasAttribute("src"));
+            Assert.IsTrue(((XmlElement) parentNode.FirstChild).HasAttribute("height"));
+            Assert.AreEqual(((XmlElement) parentNode.FirstChild).Attributes["src"].InnerText, "puppy.jpg");
+            Assert.AreEqual(((XmlElement) parentNode.FirstChild).Attributes["height"].InnerText, "720");
+        }
+    }
+}
